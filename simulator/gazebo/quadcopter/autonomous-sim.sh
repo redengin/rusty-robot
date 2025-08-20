d52044cdd9768197a3d103a566aed78387078ff0
redengin <sholstei@yahoo.com>
2025-08-19 20:49:13 -0400

    in work

--- simulator/gazebo/quadcopter/autonomous-sim.sh
+++ simulator/gazebo/quadcopter/autonomous-sim.sh
@@ -7,7 +7,7 @@ trap "trap - TERM && kill -- -$$" INT TERM EXIT
 
 robot="drone"
 # start the drone SITL
-../../../firmware/robots/gazebo-quadcopter/target/debug/autonomous $robot &
+cd ../../../firmware && cargo run --package rusty-robot-gazebo-quadcopter --bin autonomous drone &
 
 # run gazebo
 gazebo.gz sim drone-openworld.sdf
