aba5aaf81a0ba83c3a84decf9bb5a5b803ac281f
redengin <sholstei@yahoo.com>
2025-12-05 00:34:09 -0500

    in work

--- firmware/components/esp32/examples/mesh-benchmark.rs
+++ firmware/components/esp32/examples/mesh-benchmark.rs
@@ -1,10 +1,6 @@
 #![no_std]
 #![no_main]
-#![deny(
-    clippy::mem_forget,
-    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
-    holding buffers for the duration of a data transfer."
-)]
+
 // provide panic handler
 use rusty_robot_esp32::{self as _, *};
 
